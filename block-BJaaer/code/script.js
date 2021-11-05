365918b1c16769dd27a7f56bd34297f33afbc5d1
Zehan Khan <zehan9211@gmail.com>
2021-11-05 18:11:56 +0530

    handel

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -1,50 +1,56 @@
 let form = document.querySelector("form");
-let modal = document.querySelector(".modal_overlay");
-let modalinfo = document.querySelector(".modal_info");
 
-let userData = {};
+let modal = document.querySelector(".modal");
+let info = document.querySelector(".info");
 
-form.addEventListener("submit",(event) => {
-    event.preventDefault();
-    let elements = event.target.elements;
-    userData.name = elements.name.value;
-    userData.emailtext = elements.emailtext.value;
-    userData.movie = elements.gender.value;
-    userData.color = elements.color.value;
-    userData.rating = elements.range.value;
-    userData.drone = elements.drone.value;
-    userData.terms = elements.terms.value;
-    
-    modal.classList.add("open");
 
-    // let close = document.querySelector("modal_close");
-    // close.addEventListener('click', () => {
-    //     modal.classList.remove("open");
-    // })
+let userInfo = {};
 
-    displayForm(userData);
-});
+function handelSumbit(event){
+        event.preventDefault();
+        let elements = event.target.elements;
+        
+        userInfo.name = elements.name.value;
 
+        userInfo.email = elements.email[0].value;
+
+        userInfo.category = elements.gender.value;
+
+        userInfo.color = elements.email[1].value;
+
+        userInfo.rate = elements.range.value;
+
+        userInfo.genre = elements.drone.value;
+
+        console.log(elements.terms.value);
+
+        modal.classList.add("open");
+
+        displayForm(userInfo);
+
+}
+
+form.addEventListener("submit",handelSumbit);
 
 function displayForm(data = {}){
-    modalinfo.innerHTML = "";
+    info.innerHTML = "";
     let name = document.createElement('h2');
-    name.innerText = `Hello ${data.name}`;
+    name.innerText = `Hello ${userInfo.name}`;
     let email = document.createElement('p');
-    email.innerText = `Email : ${data.emailtext}`;
+    email.innerText = `Email : ${userInfo.email}`;
     let movie = document.createElement('p');
-    movie.innerText = `Movie : ${data.movie}`;
+    movie.innerText = `Movie : ${userInfo.category}`;
     let color = document.createElement('p');
-    color.innerText = `Color : ${data.color}`;
+    color.innerText = `Color : ${userInfo.color}`;
     let rating = document.createElement('p');
-    rating.innerText = `Rating : ${data.rating}`;
+    rating.innerText = `Rating : ${userInfo.rate}`;
     let drone = document.createElement('p');
-    drone.innerText = `Drone : ${data.drone}`;
+    drone.innerText = `Drone : ${userInfo.drone}`;
     let terms = document.createElement('p');
-    terms.innerText = `Terms : ${data.terms}`;
+    terms.innerText = `Terms : ${userInfo.terms}`;
 
 
-    modalinfo.append(name,email,movie,color,rating,drone,terms);    
+    info.append(name,email,movie,color,rating,drone,terms);    
     // modalinfo.append(name);
 
 }
