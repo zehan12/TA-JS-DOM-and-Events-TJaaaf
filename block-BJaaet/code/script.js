831d529510c296c4a186af6431e62f5050d8761a
Zehan Khan <zehan9211@gmail.com>
2021-11-06 19:36:00 +0530

    watch

---
+++ block-BJaaet/code/script.js
@@ -0,0 +1,15 @@
+let container = document.querySelector(".container");
+
+container.style.backgroundColor = "blue";
+container.style.padding = "  80px";
+container.style.margin = "100px 300px";
+container.style.textAlign = "center";
+
+let box = document.querySelector(".box");
+
+box.style.backgroundColor = "brown"
+box.style.padding = "20px 0 "
+
+let input = document.querySelector("input");
+
+input.style.padding = "10px 180px"
