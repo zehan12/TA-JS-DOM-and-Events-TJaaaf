7fdf2d38431d7a6f90342815bf5b458a5c6b3938
Zehan Khan <zehan9211@gmail.com>
2021-11-21 23:57:21 +0530

    keyDown

--- block-BJaafa/code/script.js
+++ block-BJaafa/code/script.js
@@ -1,64 +1,23 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('mousemove', function() {
+  var color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+  document.body.style.backgroundColor = color;
+});
 
-    const list = document.querySelector('#book-list ul');
-    const forms = document.forms;
-  
-    // delete books
-    list.addEventListener('click', (e) => {
-      if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        li.parentNode.removeChild(li);
-      }
-    });
-  
-    // add books
-    const addForm = forms['add-book'];
-    addForm.addEventListener('submit', function(e){
-      e.preventDefault();
-  
-      // create elements
-      const value = addForm.querySelector('input[type="text"]').value;
-      const li = document.createElement('li');
-      const bookName = document.createElement('span');
-      const deleteBtn = document.createElement('span');
-  
-      // add text content
-      bookName.textContent = value;
-      deleteBtn.textContent = 'delete';
-  
-      // add classes
-      bookName.classList.add('name');
-      deleteBtn.classList.add('delete');
-  
-      // append to DOM
-      li.appendChild(bookName);
-      li.appendChild(deleteBtn);
-      list.appendChild(li);
-    });
-  
-    // hide books
-    const hideBox = document.querySelector('#hide');
-    hideBox.addEventListener('change', function(e){
-      if(hideBox.checked){
-        list.style.display = "none";
-      } else {
-        list.style.display = "initial";
-      }
-    });
-  
-    // filter books
-    const searchBar = forms['search-books'].querySelector('input');
-    searchBar.addEventListener('keyup', (e) => {
-      const term = e.target.value.toLowerCase();
-      const books = list.getElementsByTagName('li');
-      Array.from(books).forEach((book) => {
-        const title = book.firstElementChild.textContent;
-        if(title.toLowerCase().indexOf(e.target.value) != -1){
-          book.style.display = 'block';
-        } else {
-          book.style.display = 'none';
-        }
-      });
-    });
-  
-  })
+let insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (event) => {
+insert.innerHTML = `
+<div class="key">
+${event.key === ' ' ? 'Space' : event.key} 
+<small>event.key</small>
+</div>
+<div class="key">
+${event.keyCode}
+<small>event.keyCode</small>
+</div>
+<div class="key">
+${event.code}
+<small>event.code</small>
+</div>
+`
+})
